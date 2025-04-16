1061fdae1ec14c61f843dedf4d4ab2fc8f4fc64b
Build-1-0-0 <africancontent807@gmail.com>
2025-04-16 21:17:34 +0300

    Create helpers.js

---
+++ worker/utils/helpers.js
@@ -0,0 +1,9 @@
+export function generateRandomString(length = 48) {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  for (let i = 0; i < length; i++) {
+    result += charset[values[i] % charset.length];
+  }
+  return result;
+}
