a4782026986dc5766657a21b346f4b4b10dee798
Build-1-0-0 <africancontent807@gmail.com>
2025-04-16 17:43:33 +0300

    Create app.jsx

---
+++ frontend/app.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SocialNest</h1>
+      <a href="/api/twitter/login">
+        <button>Connect with Twitter</button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
