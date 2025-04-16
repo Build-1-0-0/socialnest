e2e6ad770b25bf94cda6e9fdbfb38efbd605c818
Build-1-0-0 <africancontent807@gmail.com>
2025-04-16 22:07:43 +0300

    Update session.js

--- worker/utils/session.js
+++ worker/utils/session.js
@@ -1,3 +1,9 @@
+export async function saveSession(kv, state, data) {
+  await kv.put(`state:${state}`, JSON.stringify(data), {
+    expirationTtl: 300, // 5 minutes (adjust as needed)
+  });
+}
+
 export async function getSession(kv, state) {
   const data = await kv.get(`state:${state}`, { type: 'json' });
   return data;
