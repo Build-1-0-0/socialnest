584227716392454d952b09432dd6ab031a9755f9
Build-1-0-0 <africancontent807@gmail.com>
2025-04-17 14:59:59 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import app.jsx from './app.jsx';
+import App from './app.jsx';
 import './index.css'; // Tailwind CSS (if you’re using it)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
